0fa4da0117c1fd7d01155b0b450c319a006fbfcb
YifZhou <zhouyifan1012@gmail.com>
2014-11-03 16:36:14 -0700

    start writing summary doc on aperture photometry

---
+++ summary/AperturePhot1/auto/aperPhotSummary.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "aperPhotSummary"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("scrartcl" "paper=letter" "fontsize=11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("babel" "english") ("placeins" "section")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrartcl"
+    "scrartcl10"
+    "fontenc"
+    "fourier"
+    "babel"
+    "amsmath"
+    "amsfonts"
+    "amsthm"
+    "hyperref"
+    "bm"
+    "graphicx"
+    "placeins"
+    "sectsty"
+    "fancyhdr")
+   (TeX-add-symbols
+    '("horrule" 1))
+   (LaTeX-add-labels
+    "fig:subcomp"
+    "fig:lightcurve"
+    "fig:glenn")))
+
