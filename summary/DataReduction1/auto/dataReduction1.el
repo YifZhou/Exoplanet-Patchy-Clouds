824c8b00977e41dcf0a8928876cc11a21266af6d
YifZhou <zhouyifan1012@gmail.com>
2014-11-20 22:10:22 -0700

    summary for bad psf fitting finished

---
+++ summary/DataReduction1/auto/dataReduction1.el
@@ -0,0 +1,31 @@
+(TeX-add-style-hook
+ "dataReduction1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("scrartcl" "paper=letter" "fontsize=11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("babel" "english") ("placeins" "section")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrartcl"
+    "scrartcl10"
+    "fontenc"
+    "fourier"
+    "babel"
+    "amsmath"
+    "amsfonts"
+    "amsthm"
+    "hyperref"
+    "bm"
+    "graphicx"
+    "placeins"
+    "sectsty"
+    "fancyhdr")
+   (TeX-add-symbols
+    '("horrule" 1))
+   (LaTeX-add-labels
+    "fig:psf"
+    "fig:chisq"
+    "fig:corr"
+    "fig:cmd")))
+
