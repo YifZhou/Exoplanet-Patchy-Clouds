a19dbf79c120e25533de7ed53c21ea82723e04bd
YifZhou <yifzhou@email.arizona.edu>
2014-12-19 17:44:08 -0700

    cmd still need improvement

--- ABPIC_Data_Analysis/plotCMD.py
+++ ABPIC_Data_Analysis/plotCMD.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.optimize import leastsq, curve_fit
 
 
 #######Data file name, editable########
@@ -10,13 +10,8 @@ F125WInfoFile = 'f125info.csv'
 F160WInfoFile = 'f160info.csv'
 F125WFluxFile = 'Flux125_Dec01.dat'
 F160WFluxFile = 'Flux160_Dec01.dat'
-figureName = 'Flux_correlation_Dec18.pdf'
+figureName = 'CMD_Dec19.pdf'
 #######################################
-
-## linear fit
-def fitfunc(x, m, b):
-    return m * x + b
-
 
 f125DF = pd.read_csv(F125WInfoFile)
 f160DF = pd.read_csv(F160WInfoFile)
@@ -31,9 +26,33 @@ f125DF['error'] = f125err * photFlam125
 f160DF['flux'] = f160flux * photFlam160
 f160DF['error'] = f160err * photFlam160
 
+def plx2dis(plx):
+    """
+    in Dupuy 2012, parallax in marcsec
+    convert parallax to pc
+    """
+    return 1000/plx
+
+def mag2Mag(mag, dis):
+    """
+    convert magnitude to absolute magnitude
+    """
+    return mag - 5 * (np.log10(dis) - 1)
+
+def DeltaMag(flux, f0):
+    """
+    Calculate delta magnitude
+    """
+    return -2.5 * np.log10(flux/f0)
+
 if __name__ == '__main__':
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    MagTable = pd.read_csv('Dupuy2012_LT_Dwars_PhotTable.csv')
+    MagTable[['J2mag', 'H2mag', 'plx']] = MagTable[['J2mag', 'H2mag', 'plx']].astype(float)
+    MagTable['Distance'] = plx2dis(MagTable['plx'])
+    MagTable['J20mag'] = mag2Mag(MagTable['J2mag'], MagTable['Distance'])
+    MagTable['H20mag'] = mag2Mag(MagTable['H2mag'], MagTable['Distance'])
+    J0 = MagTable[MagTable['Object'] == 'AB_Pic~b']['J20mag']
+    H0 = MagTable[MagTable['Object'] == 'AB_Pic~b']['H20mag']
     flux125corr = []
     flux160corr = []
     err125corr = []
@@ -43,19 +62,17 @@ if __name__ == '__main__':
         err125corr.append(f125DF[f125DF['orbit'] == orbit_i]['error'][0:6])
         flux160corr.append(f160DF[f160DF['orbit'] == orbit_i]['flux'])
         err160corr.append(f160DF[f160DF['orbit'] == orbit_i]['error'])
-        
+
     flux125corr = np.array(flux125corr).flatten()
     err125corr = np.array(err125corr).flatten()
     flux160corr = np.array(flux160corr).flatten()
     err160corr = np.array(err160corr).flatten()
-    ax.errorbar(flux125corr, flux160corr, xerr = err125corr, yerr = err160corr, fmt = 'o', ms = 4, capthick = 1)
-    p, pcov = curve_fit(fitfunc, flux125corr, flux160corr, p0 = [1, 0], sigma = err160corr)
-    x0 = np.linspace(flux125corr.min(), flux125corr.max(), 10)
-    ax.plot(x0, p[0] * x0 + p[1])
-    ax.text(1.80e-16, 1.62e-16, r'$y = {0:.2f}x + {1:.2e}$'.format(p[0],p[1]))
-    ax.set_xlabel(r'F125W flux ($erg\,cm^{-2}s^{-1}\AA^{-1}$)')
-    ax.set_ylabel(r'F160W flux ($erg\,cm^{-2}s^{-1}\AA^{-1}$)')
-    ax.set_title('flux correlation (aperture radius = 5 pixels)')
+    JList = J0 + DeltaMag(flux125corr, flux125corr.mean())
+    HList = H0 + DeltaMag(flux160corr, flux160corr.mean())
+    fig = plt.figure(figsize = (6, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(JList - HList, JList, '+', mew = 1.0)
+    ax.plot(magTable['J20mag'] - magTable['H20mag'], magTable['J20mag'], '.', mew = 0.8, color = '0.8', zorder = 0)
+    ax.invert_yaxis()
     fig.tight_layout()
-    #fig.savefig(figureName)
-    print p, pcov
+    fig.savefig(figureName)
