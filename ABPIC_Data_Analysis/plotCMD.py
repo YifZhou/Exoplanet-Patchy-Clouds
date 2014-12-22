3796a86d00a6ad33ef771af9df0bf07f7374d40b
YifZhou <zhouyifan1012@gmail.com>
2014-12-22 10:50:22 -0700

    cmd plot

--- ABPIC_Data_Analysis/plotCMD.py
+++ ABPIC_Data_Analysis/plotCMD.py
@@ -51,8 +51,8 @@ if __name__ == '__main__':
     MagTable['Distance'] = plx2dis(MagTable['plx'])
     MagTable['J20mag'] = mag2Mag(MagTable['J2mag'], MagTable['Distance'])
     MagTable['H20mag'] = mag2Mag(MagTable['H2mag'], MagTable['Distance'])
-    J0 = MagTable[MagTable['Object'] == 'AB_Pic~b']['J20mag']
-    H0 = MagTable[MagTable['Object'] == 'AB_Pic~b']['H20mag']
+    J0 = MagTable[MagTable['Object'] == 'AB_Pic~b']['J20mag'].values
+    H0 = MagTable[MagTable['Object'] == 'AB_Pic~b']['H20mag'].values
     flux125corr = []
     flux160corr = []
     err125corr = []
@@ -64,6 +64,7 @@ if __name__ == '__main__':
         err160corr.append(f160DF[f160DF['orbit'] == orbit_i]['error'])
 
     flux125corr = np.array(flux125corr).flatten()
+    print flux125corr/flux125corr.mean()
     err125corr = np.array(err125corr).flatten()
     flux160corr = np.array(flux160corr).flatten()
     err160corr = np.array(err160corr).flatten()
@@ -72,7 +73,20 @@ if __name__ == '__main__':
     fig = plt.figure(figsize = (6, 8))
     ax = fig.add_subplot(111)
     ax.plot(JList - HList, JList, '+', mew = 1.0)
-    ax.plot(magTable['J20mag'] - magTable['H20mag'], magTable['J20mag'], '.', mew = 0.8, color = '0.8', zorder = 0)
+    ax.plot(MagTable['J20mag'] - MagTable['H20mag'], MagTable['J20mag'], '.', mew = 0.8, color = '0.8', zorder = 0)
+    #ax.set_ylim([12, 13])
     ax.invert_yaxis()
+    ax.set_xlabel('Color (J-H)')
+    ax.set_ylabel('Absolute J Magnitude')
+
+    ax_abpic = fig.add_axes([0.2, 0.6, 0.32, 0.32])
+    ax_abpic.plot(JList - HList, JList, '+', mew = 1.0)
+    ax_abpic.set_aspect('equal')
+    # ax_abpic.plot(MagTable['J20mag'] - MagTable['H20mag'], MagTable['J20mag'], '.', mew = 0.8, color = '0.8', zorder = 0)
+    ax_abpic.set_xlim([1.46, 1.52])
+    ax_abpic.set_ylim([12.84, 12.9])
+    ax_abpic.xaxis.set_ticks([1.46, 1.48, 1.50, 1.52])
+    ax_abpic.yaxis.set_ticks([12.84,12.86,12.88, 12.90])
+    ax_abpic.invert_yaxis()
     fig.tight_layout()
     fig.savefig(figureName)
