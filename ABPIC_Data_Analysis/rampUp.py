63e076e7294e4a18ec63922562d2338c8707f1c6
YifZhou <zhouyifan1012@gmail.com>
2015-02-12 13:30:45 -0700

    one by one and calm down

--- ABPIC_Data_Analysis/rampUp.py
+++ ABPIC_Data_Analysis/rampUp.py
@@ -7,10 +7,13 @@ import numpy as np
 if __name__ == '__main__':
     dataDIR = '../data/ABPIC-B'
     fn = sys.argv[1]
+    imaFn = fn + '_ima.fits'
+    fltFn = fn + '_flt.fits'
     x = int(sys.argv[2]) 
     y = int(sys.argv[3]) # do not use x or y, they are confusing
     dim1, dim2 = y, x
-    ima = fits.open(os.path.join(dataDIR, fn))
+    ima = fits.open(os.path.join(dataDIR, imaFn))
+    flt = fits.open(os.path.join(dataDIR, fltFn))
     nSamp = ima['primary'].header['nsamp']
     count = np.zeros(nSamp)
     sampTime = np.zeros(nSamp)
@@ -22,11 +25,15 @@ if __name__ == '__main__':
         count[i+1] = ima['sci', samp_i].data[dim1, dim2] * sampTime[i + 1] + count[0]
 
     plt.close('all')
-    plt.plot(sampTime, count, marker = '+')
+    plt.plot(sampTime, count, marker = '+', ms = 12)
+    plt.plot(flt['primary'].header['exptime'], flt['sci'].data[dim1-5, dim2-5] * flt['primary'].header['exptime'] + count[0], 'o', ms = 12)
     print sampTime, count
+    print flt['sci'].data[dim1-5, dim2-5], flt['sci'].data[dim1-5, dim2-5] * flt['primary'].header['exptime'] + count[0]
     ax = plt.gca()
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Count (e$^-$)')
+    ima.close()
+    flt.close()
     plt.show()
 
     
