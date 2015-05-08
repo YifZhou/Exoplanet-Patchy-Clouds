c571f93f5792b23cc90f1023220d144920222e08
YifZhou <zhouyifan1012@gmail.com>
2015-05-07 18:59:47 -0700

    data reduction part finished first version

--- summary/ABPIC-PAPER/auto/msv1.el
+++ summary/ABPIC-PAPER/auto/msv1.el
@@ -2,12 +2,32 @@
  "msv1"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("emulateapj" "iop")))
+                     '(("emulateapj" "apj")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("hyperref" "breaklinks" "colorlinks" "citecolor=blue" "linkcolor=magenta")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "emulateapj"
     "emulateapj10"
     "graphicx"
     "amssymb"
-    "amsmath")))
+    "amsmath"
+    "natbib"
+    "hyperref")
+   (TeX-add-symbols
+    "ima"
+    "flt"
+    "eps")
+   (LaTeX-add-labels
+    "fig:image")
+   (LaTeX-add-bibliographies
+    "ref.bib")))
 
---
+++ summary/ABPIC-PAPER/auto/ref.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "ref"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Triaud2015"
+    "Swain2013"
+    "Deming2013"
+    "McQuillan2013"
+    "Deming2014"
+    "Mandell2013"
+    "Berta2012")))
+
